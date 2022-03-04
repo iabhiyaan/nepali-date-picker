bf26e55ed26542622256f605217253e48f4dfd32
Abhiyan <iabhiyaan@gmail.com>
2022-03-04 12:30:22 +0545

    :zap: minor chnages

---
+++ index.js
@@ -0,0 +1,10 @@
+import NepaliDatePicker from "./src/components/NepaliDatePicker";
+import DateRangePicker from "./src/components/DateRangePicker";
+
+import useDate from "./src/composables/useDate";
+
+export {
+    NepaliDatePicker,
+    DateRangePicker,
+    useDate
+}
