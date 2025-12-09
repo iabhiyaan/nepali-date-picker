52421bedde78e6dfa1ad018c15e0a340839cf675
Abhiyan Shrestha <iabhiyaan@gmail.com>
2025-12-09 23:07:55 +0545

    refactor: refactored to use script setup, defineProps, defineModel

--- src/components/DateRangePicker.vue
+++ src/components/DateRangePicker.vue
@@ -1,53 +1,39 @@
-<script>
-import NepaliDatePicker from '../components/NepaliDatePicker.vue';
+<script setup>
+import NepaliDatePicker from "../components/NepaliDatePicker.vue";
 
-export default {
-  components: {
-    NepaliDatePicker,
-  },
-  props: {
-    fromDate: {},
-    toDate: {},
-    format: {type: String, default: "yyyy-mm-dd"},
-    calenderType: {type: String, default: "Nepali"},
-    yearSelect: {type: Boolean, default: true},
-    monthSelect: {type: Boolean, default: true},
-    classValue: {type: String, default: ""},
-    placeholder: {type: String, default: ""},
-  },
-  setup(props, {emit}) {
-    function fromDateFn(val) {
-      emit('update:fromDate', val)
-    }
+defineOptions({
+  inheritAttrs: false
+})
 
-    function toDateFn(val) {
-      emit('update:toDate', val)
-    }
+const props = defineProps({
+  fromDate: {},
+  toDate: {},
+  format: { type: String, default: "yyyy-mm-dd" },
+  calenderType: { type: String, default: "Nepali" },
+  yearSelect: { type: Boolean, default: true },
+  monthSelect: { type: Boolean, default: true },
+  classValue: { type: String, default: "" },
+  placeholder: { type: String, default: "" },
+});
 
-    return {
-      fromDateFn,
-      toDateFn
-    }
-  }
-};
+const fromDate = defineModel("fromDate");
+const toDate = defineModel("toDate");
 </script>
 
 <template>
-  <div style="display: flex; margin-bottom: 40px;">
+  <div style="display: flex; margin-bottom: 40px">
     <NepaliDatePicker
-        v-bind="{...$attrs, ...$props}"
-        classValue="form-control"
-        placeholder="From Date"
-        :model-value="fromDate"
-        @update:modelValue="fromDateFn"
+      v-model="fromDate"
+      v-bind="{ ...$attrs, ...props }"
+      classValue="form-control"
+      placeholder="From Date"
     />
     <NepaliDatePicker
-        v-bind="{...$attrs, ...$props}"
-        style="margin-left: 30px;"
-        placeholder="To Date"
-        classValue="form-control"
-        :model-value="toDate"
-        @update:modelValue="toDateFn"
+      v-model="toDate"
+      v-bind="{ ...$attrs, ...props }"
+      style="margin-left: 30px"
+      placeholder="To Date"
+      classValue="form-control"
     />
   </div>
 </template>
